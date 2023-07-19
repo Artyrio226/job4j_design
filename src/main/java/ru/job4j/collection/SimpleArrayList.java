e915937121f1d9effabd03ab479be795a46c6d52
Artyrio226 <artur_sar_80@mail.ru>
2023-07-19 17:01:00 +0300

    1. Динамический список на массиве. [#158  [#158]]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,79 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            grow();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        T element = container[Objects.checkIndex(index, size)];
+        container[index] = newValue;
+        return element;
+    }
+
+    @Override
+    public T remove(int index) {
+        T element = container[Objects.checkIndex(index, size)];
+        System.arraycopy(container, index + 1, container, index, size - index - 1);
+        container[size - 1] = null;
+        size--;
+        modCount++;
+        return element;
+    }
+
+    @Override
+    public T get(int index) {
+        return container[Objects.checkIndex(index, size)];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            final int expectedModCount = modCount;
+            int point = 0;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[point++];
+            }
+        };
+    }
+
+    private void grow() {
+        container = Arrays.copyOf(container,
+                (container.length > 0 ? container.length : 1) * 2);
+    }
+}

36ae02696ffd041c92ba57d82bc156c8ba3b7a9e
Artyrio226 <artur_sar_80@mail.ru>
2023-08-02 13:53:19 +0300

    6. Читаем файл конфигурации [#858  [#858]]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithComment() {
+        String path = "./data/test1.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("#ok")).isEqualTo(null);
+        assertThat(config.value("ok1")).isEqualTo("Russia");
+        assertThat(config.value("test")).isEqualTo("valid=");
+        assertThat(config.value("key")).isEqualTo("val#13");
+    }
+
+    @Test
+    void whenFileWithoutComment() {
+        String path = "./data/test2.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load).isInstanceOf(IllegalArgumentException.class);
+    }
+}

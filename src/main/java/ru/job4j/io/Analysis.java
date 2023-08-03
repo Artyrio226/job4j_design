505bb797386f6c1df5114d4f15a856a28a3df789
Artyrio226 <artur_sar_80@mail.ru>
2023-08-03 14:06:52 +0300

    7. Анализ доступности сервера [#859  [#859 #434160]]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,18 +6,17 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
-            boolean off = false;
+            boolean off = true;
             while (in.ready()) {
                 String[] s = in.readLine().split(" ");
-                String k = s[0];
-                String v = s[1];
-                if (("400".equals(k) || "500".equals(k)) && !off) {
-                    out.append(v).append(";");
-                    off = true;
-                }
-                if (("200".equals(k) || "300".equals(k)) && off) {
-                    out.append(v).append(";").append(System.lineSeparator());
-                    off = false;
+                boolean status = "400".equals(s[0]) || "500".equals(s[0]);
+                if (status == off) {
+                    out.append(s[1])
+                            .append(";")
+                            .append(off
+                                    ? ""
+                                    : System.lineSeparator());
+                    off = !off;
                 }
         }
         } catch (IOException e) {

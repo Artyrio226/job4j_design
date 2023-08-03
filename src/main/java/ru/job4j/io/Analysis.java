c403e522d27e68c77f44353afffb36a286cc50fe
Artyrio226 <artur_sar_80@mail.ru>
2023-08-03 10:02:24 +0300

    7. Анализ доступности сервера [#859  [#859 #434160]]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,28 +1,25 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.List;
-import java.util.StringJoiner;
 
 public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
-            List<String> list = in.lines().toList();
-            StringJoiner sj = new StringJoiner(";");
-            for (String l: list) {
-                Object[] tmp = l.split(" ");
-                String k = tmp[0].toString();
-                String v = tmp[1].toString();
-                if (("400".equals(k) || "500".equals(k)) && sj.length() == 0) {
-                    sj.add(v);
+            boolean off = false;
+            while (in.ready()) {
+                String[] s = in.readLine().split(" ");
+                String k = s[0];
+                String v = s[1];
+                if (("400".equals(k) || "500".equals(k)) && !off) {
+                    out.append(v).append(";");
+                    off = true;
                 }
-                if (("200".equals(k) || "300".equals(k)) && sj.length() != 0) {
-                    sj.add(v);
-                    out.write(sj + ";" + System.lineSeparator());
-                    sj = new StringJoiner(";");
+                if (("200".equals(k) || "300".equals(k)) && off) {
+                    out.append(v).append(";").append(System.lineSeparator());
+                    off = false;
                 }
-            }
+        }
         } catch (IOException e) {
             e.printStackTrace();
         }

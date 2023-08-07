56ead33c5beed1eab6a06cf7305fb64e825a7857
Artyrio226 <artur_sar_80@mail.ru>
2023-08-07 15:29:19 +0300

    11. Поиск дубликатов [#315066  [#315066]]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,40 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<Path>> map = new HashMap<>();
+
+    public void getDuplicates() {
+        map.forEach((key, value) -> {
+            if (value.size() > 1) {
+                System.out.printf("%s - %d%n", key.getName(), key.getSize());
+                value.forEach(a -> System.out.println(a.toAbsolutePath()));
+            }
+        });
+    }
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fp = new FileProperty(attrs.size(), file.toFile().getName());
+        if (map.containsKey(fp)) {
+            map.computeIfPresent(fp, (a, b) -> {
+                b.add(file);
+                return b;
+            });
+        } else {
+            List<Path> tmp = new LinkedList<>();
+            tmp.add(file);
+            map.put(fp, tmp);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}

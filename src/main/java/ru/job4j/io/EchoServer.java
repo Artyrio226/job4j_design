51ab4fb045d66cfdfa5c9b55f4bf62ebac14a3e2
Artyrio226 <artur_sar_80@mail.ru>
2023-08-12 19:03:03 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,17 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class EchoServer {
+    private static String answer(String str) {
+        String tmp = Arrays
+                .stream(str.split(" "))
+                .filter(a -> a.contains("msg="))
+                .findFirst()
+                .orElse("No message.");
+        return tmp.substring(tmp.indexOf("=") + 1);
+    }
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,12 +22,18 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String tmp = "";
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("msg=Bye")) {
-                            server.close();
+                        if (str.contains("?msg=")) {
+                            tmp = str;
                         }
                     }
+                    String rsl = answer(tmp);
+                    if ("Exit".equals(rsl)) {
+                        server.close();
+                    }
+                    out.write((rsl + "\r\n").getBytes());
                     out.flush();
                 }
             }

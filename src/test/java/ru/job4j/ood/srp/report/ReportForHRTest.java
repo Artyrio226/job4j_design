99ed60ad7d92a841dee427ed6112fed7ed7cdd43
Artyrio226 <artur_sar_80@mail.ru>
2023-10-11 13:35:00 +0300

    1. Отчеты. [#850]

---
+++ src/test/java/ru/job4j/ood/srp/report/ReportForHRTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemStore;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ReportForHRTest {
+
+    @Test
+    public void whenRFHRGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker1 = new Employee("Ivan", now, now, 100);
+        Employee worker2 = new Employee("Anton", now, now, 120);
+        store.add(worker1);
+        store.add(worker2);
+        Report engine = new ReportForHR(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Salary;")
+                .append(System.lineSeparator())
+                .append(worker2.getName()).append(" ")
+                .append(worker2.getSalary())
+                .append(System.lineSeparator())
+                .append(worker1.getName()).append(" ")
+                .append(worker1.getSalary())
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+}
